4d59861372d62193265dba309da2422eb69faafc
SOFEWARELZP <243510418@qq.com>
2017-04-12 10:13:43 +0800

    链家数据爬虫
    链家网数据

---
+++ 1/7.py
@@ -0,0 +1,43 @@
+import urllib
+import urllib2
+import re
+import os
+class spider:
+    def __init__(self):
+        self.url = 'http://bj.lianjia.com/zufang/'
+    def getpage(self,pageindex):
+        URL = self.url+'pg'+str(pageindex)+'/'
+        request = urllib2.Request(URL)
+        response = urllib2.urlopen(request)
+        return response.read().decode('utf-8')
+    def mkdir(self, path):
+        path=path.strip()
+        isExists= os.path.exists(path)
+        if not isExists:
+            os.makedirs(path)
+            return True
+        else:
+            return False
+    def savetxt(self, content, filename):
+        self.mkdir('bj')
+        filename= 'bj'+'/'+str(filename)+'.txt'
+        f=open(filename, "w+")
+        f.write(content.encode('utf-8'))
+        f.close()
+    def getcontent(self, pageindex):
+        response = self.getpage(pageindex)
+        pattern = re.compile('<a target="_blank" href="http://bj.lianjia.com/zufang/(.*?).html" title="(.*?)">(.*?)</a>', re.S)
+        items = re.findall(pattern, response)
+        lines=''
+        for item in items:
+            print item[1]
+            lines=lines+item[1]+'\n'
+        self.savetxt(lines, pageindex)
+    def pagescontent(self,pagefirst, pageend):
+        for value in range(pagefirst, pageend):
+            self.getcontent(value)
+spider = spider()
+spider.pagescontent(1, 101)
+
+
+
